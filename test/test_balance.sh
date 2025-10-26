f4ea0fc63add065252252165322edccf77b5668d
Edil Medeiros <jose.edil@gmail.com>
2025-10-26 19:00:13 -0300

    Adjust graders

--- test/test_balance.sh
+++ test/test_balance.sh
@@ -1,3 +1,14 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# Run student solution
 result=$(OPENSSL_CONF=./test/openssl.cnf bash ./solution/run_balance.sh | tail -n 1)
-echo $result
-grep -xq "$result" ./test/wallet_balances.txt && echo "PASS" || echo "FAIL"
+echo "$result"
+
+# Check against expected list of valid balances
+if grep -Fxq "$result" ./test/wallet_balances.txt; then
+  echo "PASS"
+else
+  echo "FAIL"
+  exit 1
+fi
--- test/test_spend.sh
+++ test/test_spend.sh
@@ -1,8 +1,26 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
 result=$(OPENSSL_CONF=./test/openssl.cnf bash ./solution/run_spend.sh)
-echo $result
+echo "$result"
+
 tx1=$(echo "$result" | tail -n 2 | head -n 1)
 tx2=$(echo "$result" | tail -n 1)
 
-bitcoin-cli -signet testmempoolaccept "[\"$tx1\"]"
-bitcoin-cli -signet testmempoolaccept "[\"$tx1\",\"$tx2\"]"
+# Run first test
+out1=$(bitcoin-cli -signet testmempoolaccept "[\"$tx1\"]" | jq -r '.[0].allowed')
+
+if [[ "$out1" == "true" ]]; then
+  # TX1 was accepted. Now test TX1 + TX2 together
+  out2=$(bitcoin-cli -signet testmempoolaccept "[\"$tx1\",\"$tx2\"]" | jq -r '.[1].allowed')
+  if [[ "$out2" == "true" ]]; then
+    echo "PASS"
+  else
+    echo "FAIL ❌ TX2 rejected"
+    exit 1
+  fi
+else
+  echo "FAIL ❌ TX1 rejected"
+  exit 1
+fi
 
